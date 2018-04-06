971a3b242ff98b4635374ae944e92a5fb4b0fad9
Jude Kuti <judekuti@gmail.com>
2018-04-06 16:40:26 -0500

    Import components and include in decorator as imports and exports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,16 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
-
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { MatGridListModule } from '@angular/material/grid-list';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MatButtonModule, MatCheckboxModule, MatGridListModule
   ],
+  exports: [MatGridListModule, MatButtonModule, MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
