ebd5ca59bd92b4f701296456b809e854ebec51c5
Jude Kuti <judekuti@gmail.com>
2018-04-06 17:11:07 -0500

    Get images into assets and import mat-toolbar module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, MatButtonModule, MatCheckboxModule, MatGridListModule
+    BrowserModule, MatButtonModule, MatCheckboxModule, MatToolbarModule, MatGridListModule
   ],
-  exports: [MatGridListModule, MatButtonModule, MatCheckboxModule],
+  exports: [MatGridListModule, MatButtonModule, MatToolbarModule, MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
