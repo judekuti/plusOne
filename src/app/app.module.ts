62c5232210f1c0767ae5211e7d6ca82cb9131f13
Jude Kuti <judekuti@gmail.com>
2018-04-06 18:46:35 -0500

    Install Flex-Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, MatButtonModule, MatCheckboxModule, MatToolbarModule, MatGridListModule
+    BrowserModule,BrowserAnimationsModule, MatButtonModule, MatCheckboxModule,MatSidenavModule, MatToolbarModule, MatGridListModule
   ],
-  exports: [MatGridListModule, MatButtonModule, MatToolbarModule, MatCheckboxModule],
+  exports: [MatGridListModule, MatButtonModule,MatSidenavModule, MatToolbarModule, MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
