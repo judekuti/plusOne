d3101c73592c71067d7a788661dc1e4599b9e1f7
Jude Kuti <judekuti@gmail.com>
2018-04-06 22:26:15 -0500

    Use ViewChild decorator to open sidenav on clicking info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ModelsService } from './models.service';
 import { Model } from './models';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
   models: Model[]=[];
   selectedModel: Model;
+  @ViewChild('sidenav') sidenav: MatSidenavModule;
+
+
   constructor(private modelsService: ModelsService){
 
   }
@@ -23,5 +25,7 @@ export class AppComponent {
           // we select the model by creating a new property selectedModel within APpComponent
           this.selectedModel= model;
       // 2. open sidenav
+          // In order to open the side nav it has to be referenced in component. Using ViewChild decorator will query the component's template from within the component. Firstly we create a local template variable #sidenav in the sidenav then we connect the #sidenav in template using ViewChild as above
+          this.sidenav.open();
   }
 }
