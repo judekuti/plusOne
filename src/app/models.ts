9fca03304c5ad8e95ff0d109ceebb6233388e9cb
Jude Kuti <judekuti@gmail.com>
2018-04-06 20:23:29 -0500

    Create models interface for schema and models.service to hold array of models details

---
+++ src/app/models.ts
@@ -0,0 +1,15 @@
+export class Model {
+  name: string;
+  status: string;
+  about: string;
+  messages: Message[];
+
+  rows: number;
+  cols: number;
+}
+
+export class Message {
+  who: string;
+  content: string;
+}
+
