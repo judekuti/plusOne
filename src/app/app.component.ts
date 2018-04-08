d47b129f149b6c7a4c133a72ff6e251a7d7f47e9
Jude Kuti <judekuti@gmail.com>
2018-04-08 17:58:49 -0500

    Conclude with dialog...though a few bugs to be fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { ModelsService } from './models.service';
 import { Model } from './models';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatDialogConfig} from '@angular/material';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -31,6 +34,8 @@ export class AppComponent implements OnInit {
           // In order to open the side nav it has to be referenced in component. Using ViewChild decorator will query the component's template from within the component. Firstly we create a local template variable #sidenav in the sidenav then we connect the #sidenav in template using ViewChild as above
           this.sidenav.open();
   }
+
+
   addMessage(){
       //Open dialog to add message
       // 1. create dialog config
@@ -42,9 +47,32 @@ export class AppComponent implements OnInit {
       dialog.selectedModel = this.selectedModel;
       console.log(dialog);
   }
-
-  @Component({
-      selector: 'add-message';
-      template: '<p> empty component</p>'
-  })
 }
+  @Component({
+      selector: 'add-message',
+      template: `
+      <form (submit)="addMessage()">
+      <mat-input-container>
+      <input matInput name="message" [(ngModel)]= "message" placeholder="Message">
+      </mat-input-container>
+      <button mat-raised-button color="accent">Add Message</button>
+      </form>
+      `
+  })
+
+  export class AddMessageComponent{
+      message = '';
+
+      constructor(private matDialogRef: MatDialogRef<AddMessageComponent>){}
+
+      addMessage(){
+          //1. create message
+          const newMessage = { who: 'John Doe', content: this.message};
+          //2. add message to selected model
+          const selectedModel : Model = (<any>this.matDialogRef.componentInstance).selectedModel;
+          selectedModel.messages.push(newMessage);
+          //3. close dialog
+          this.matDialogRef.close();
+      }
+  }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
+import { AppComponent, AddMessageComponent } from './app.component';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -13,17 +13,19 @@ import { MatIconModule } from '@angular/material/icon';
 import { ModelsService } from './models.service';
 import {MatCardModule} from '@angular/material/card';
 import {MatDialogModule} from '@angular/material';
-
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, AddMessageComponent
   ],
+
   imports: [
     BrowserModule,BrowserAnimationsModule, MatButtonModule, MatCheckboxModule,MatSidenavModule, MatToolbarModule, MatGridListModule, FlexLayoutModule,
-        MatTabsModule, MatIconModule, MatCardModule, MatDialogModule
+        MatTabsModule, MatIconModule, MatCardModule, MatDialogModule, MatInputModule, FormsModule
   ],
-  exports: [MatGridListModule, MatButtonModule,MatSidenavModule, MatToolbarModule, MatCheckboxModule, MatTabsModule, MatIconModule, MatCardModule, MatDialogModule],
+  exports: [MatGridListModule, MatButtonModule,MatSidenavModule, MatToolbarModule, MatCheckboxModule, MatTabsModule, MatIconModule, MatCardModule, MatDialogModule, MatInputModule, FormsModule],
   providers: [ModelsService],
   bootstrap: [AppComponent]
 })
